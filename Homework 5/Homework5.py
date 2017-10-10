be84dc9e23c69158d34ee4154e3213d9319b1de3
Daniella Raz <daniellaraz@gmail.com>
2017-10-10 18:09:54 -0400

    Final Homework 5

--- Homework 5/Homework5.py
+++ Homework 5/Homework5.py
@@ -1,11 +1,17 @@
 import re
 
 lst = []
-fh = open("file.txt", "r")
+total = 0
+fh = open("regex_sum_32305.txt", "r")
 for line in fh:
-    a = re.findall('[0-9]+', 'line')
-    if a != []:
+    a = re.findall('[0-9]+', line)
+
+    if a != lst:
         lst.extend(a)
-    total = 0
-    for a in lst:
-        total = total + a
+
+list_of_ints = map(int, lst)
+
+for a in list_of_ints:
+    total += a
+
+print(total)
