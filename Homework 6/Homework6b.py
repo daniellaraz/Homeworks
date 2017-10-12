ea17e1286516e6dd8841e00ee2a0e2de66721ea4
Daniella Raz <daniellaraz@gmail.com>
2017-10-12 10:35:12 -0400

    updated Homework6b

--- Homework 6/Homework6b.py
+++ Homework 6/Homework6b.py
@@ -6,26 +6,18 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input("Enter - ")
+url = 'http://py4e-data.dr-chuck.net/known_by_Talorcan.html'
 html = urlopen(url, context=ctx).read()
-
 soup = BeautifulSoup(html, "html.parser")
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
-#lst = []
-#i = 0
-#count = 7
-#while i < count:
-'''
-tags = soup('a')
-for tag in tags:
-#    print('TAG: ', tag)
-    print('URL: ', tag.get('href', None))
-    print('URL: at position 18', tag.get('href', ))
-    #print('Contents: ', tag.contents[0])
-    #print('Attrs: ', tag.attrs)
-#all_links = soup.find_all("a")
-#all_links[3]
-'''
-#use beautiful soup to read in the position 3 link
+count = 0
+position = 18
+
+while count < 7:
+
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
+    tags = soup('a')
+    url =  tags[position-1].get('href', None)
+    print(url)
+    count += 1
